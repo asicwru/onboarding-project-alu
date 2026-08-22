319273da1de43f9f32ac23ff67215c66766bda84
JP <johnp.magbitang@gmail.com>
2026-08-22 10:43:30 -0400

    make ALU barebones template

--- rtl/alu.sv
+++ rtl/alu.sv
@@ -1,1 +1,24 @@
+module alu (
+    input  logic [31:0] a,
+    input  logic [31:0] b,
+    input  logic [3:0]  op,
+    output logic [31:0] y
+);
 
+always_comb begin
+    case (op)
+        // ADD
+        // SUB
+        // AND
+        // OR
+        // XOR
+        // NOT
+        // SLL
+        // SRL
+        // SLT
+        // EQ
+        default:
+    endcase
+end
+
+endmodule
