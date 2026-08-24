ba65f1f8c7c5b6206de307d031d87ddbbb18f662
john-paul-sm <johnp.magbitang@gmail.com>
2026-08-23 22:12:51 -0400

    defined TODO's in the RTL file

--- rtl/alu.sv
+++ rtl/alu.sv
@@ -1,24 +1,43 @@
 module alu (
     input  logic [31:0] a,
     input  logic [31:0] b,
-    input  logic [3:0]  op,
-    output logic [31:0] y
+    input  logic [3:0]  opcode,
+    output logic [31:0] result
 );
+
+/*
+The opcode for the add operation is 4'h0. Fill in the rest of the local parameters.
+*/
+
+localparam ADD = 4'h0;
+localparam SUB =
+localparam AND =
+localparam OR =
+localparam XOR =
+localparam NOT =
+localparam SLL =
+localparam SRL =
+localparam SLT =
+localparam EQ =
+
+/*
+The combinational statement for the ADD operation is given. Fill in the rest.
+*/
 
 always_comb begin
     case (op)
-        // ADD
-        // SUB
-        // AND
-        // OR
-        // XOR
-        // NOT
-        // SLL
-        // SRL
-        // SLT
-        // EQ
-        default:
+        4'h0: result = a + b;   // ADD
+                                // SUB
+                                // AND
+                                // OR
+                                // XOR
+                                // NOT
+                                // SLL
+                                // SRL
+                                // SLT
+                                // EQ
+        default: result = 32'b0; // Invalid opcode
     endcase
 end
 
-endmodule
+endmodule     
